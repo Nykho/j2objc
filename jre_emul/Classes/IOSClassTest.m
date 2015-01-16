59776e040e53ec813e6b67c8258c7a033dbf7cc0
Michael Voong <michael@laundrapp.com>
2015-01-16 14:49:58 +0000

    Added tests for IOSClass new isEqual and hash methods

--- jre_emul/Classes/IOSClassTest.m
+++ jre_emul/Classes/IOSClassTest.m
@@ -25,6 +25,7 @@
 #import "IOSObjectArray.h"
 #import "java/lang/Double.h"
 #import "java/util/Arrays.h"
+#import "java/lang/ref/WeakReference.h"
 
 #import <XCTest/XCTest.h>
 
@@ -44,6 +45,27 @@
                                   type:[IOSClass getClass]];
   id method = [arraysClass getMethod:@"binarySearch" parameterTypes:argTypes];
   XCTAssertNotNil(method, @"Arrays.binarySearch(double[], double) not found");
+}
+
+- (void)testHash {
+  IOSClass *arrayClass = [IOSClass classWithClass:[IOSArray class]];
+  XCTAssertTrue([arrayClass hash] == [arrayClass hash], @"IOSClasses should have same hash if they are the same object");
+  XCTAssertEqual([arrayClass hash], [[IOSClass classWithClass:[IOSArray class]] hash], @"Hash should be same for same class");
+  XCTAssertFalse([arrayClass hash] == [[IOSClass classWithClass:[NSObject class]] hash], @"IOSClasses should not have same hash if the classes are not referent subclasses of each other");
+
+  id array = [[[IOSArray alloc] init] autorelease];
+  [[[JavaLangRefWeakReference alloc] initWithId:array] autorelease];
+  XCTAssertTrue([[array getClass] hash] == [arrayClass hash], @"Hash of referent subclass should be same as original class");
+}
+
+- (void)testIsEqual {
+  IOSClass *exampleClass = [IOSClass classWithClass:[IOSArray class]];
+  XCTAssertTrue([exampleClass isEqual:exampleClass], @"IOSClasses should be equal if they are the same object");
+  XCTAssertTrue([exampleClass isEqual:[IOSClass classWithClass:[NSObject class]]], @"IOSClasses should be equal if they are objects of the same class");
+  
+  id object = [[[NSObject alloc] init] autorelease];
+  [[[JavaLangRefWeakReference alloc] initWithId:object] autorelease];
+  XCTAssertTrue([[object getClass] isEqual:exampleClass], @"IOSClasses should be equal if one is a referent subclass of the other");
 }
 
 @end
